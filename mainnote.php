e55d136d20a9a921f77b2e1f11f7541ca8c36f73
MalkuSR <m.s.malik3105@gmail.com>
2024-09-11 21:01:26 +0500

    theme finaly completed

--- mainnote.php
+++ mainnote.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['user_id'])) {
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['theme'])) {
         $_SESSION['theme'] = $_POST['theme'];
-        echo $_SESSION['theme']; 
+        echo $_SESSION['theme'];
     }
 }
 ?>
@@ -26,19 +26,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-     <!-- fevicon -->
-     <link rel="apple-touch-icon" sizes="180x180" href="assets/images/png-transparent-computer-icons-notepad-others-thumbnail-removebg-preview.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/png-transparent-computer-icons-notepad-others-thumbnail-removebg-preview.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/png-transparent-computer-icons-notepad-others-thumbnail-removebg-preview.png">
+    <!-- fevicon -->
+    <link rel="apple-touch-icon" sizes="180x180"
+        href="assets/images/png-transparent-computer-icons-notepad-others-thumbnail-removebg-preview.png">
+    <link rel="icon" type="image/png" sizes="32x32"
+        href="assets/images/png-transparent-computer-icons-notepad-others-thumbnail-removebg-preview.png">
+    <link rel="icon" type="image/png" sizes="16x16"
+        href="assets/images/png-transparent-computer-icons-notepad-others-thumbnail-removebg-preview.png">
     <link rel="manifest" href="/site.webmanifest">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
     <title>iNotePad</title>
 </head>
 
-<body id="body" class="body <?php echo isset($_SESSION['theme']) && $_SESSION['theme'] == 'light' ? 'light-mode' : 'dark-mode'; ?>">
+<body
+    class="body <?php echo isset($_SESSION['theme']) && $_SESSION['theme'] == 'light' ? 'light-mode' : 'dark-mode'; ?>">
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
         <div class="container">
@@ -58,7 +64,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
-                    <button id="themeToggleBtn" class="btn btn-outline-secondary me-2">Toggle Dark Mode</button>
                     <div class="dropdown text-end">
                         <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle"
                             data-bs-toggle="dropdown" aria-expanded="false">
@@ -68,6 +73,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         <ul class="dropdown-menu text-small" style="">
                             <li><a class="dropdown-item" href="#">Profile</a></li>
                             <li><a class="dropdown-item" href="#">Settings</a></li>
+                            <li><a id="themeToggleBtn"><i
+                            class="fa-solid <?php echo isset($_SESSION['theme']) && $_SESSION['theme'] == 'dark' ? 'fa-toggle-off' : 'fa-toggle-on'; ?>"></i><span class="mx-1" style="cursor:pointer;">Theme</span></a></li>
                             <li>
                                 <hr class="dropdown-divider">
                             </li>
@@ -86,12 +93,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <h3>Add Your Notes</h3>
                 <div class="mb-3">
                     <label for="title" class="form-label">Title</label>
-                    <input type="text" class="form-control" name="title" id="title" aria-describedby="emailHelp" required >
+                    <input type="text" class="form-control" name="title" id="title" aria-describedby="emailHelp"
+                        required>
                 </div>
                 <div class="mb-3">
                     <label for="description" class="form-label">Description</label>
-                    <textarea class="description form-control" name="description" id="description" rows="6"
-                        cols="90" required ></textarea>
+                    <textarea class="description form-control" name="description" id="description" rows="6" cols="90"
+                        required></textarea>
                 </div>
                 <div class="mb-3">
                     <label for="tag" class="form-label">Tag</label>
@@ -106,8 +114,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <h3>All Notes</h3>
         </div>
 
-        <?php 
-         include("addNote.php");
+        <?php
+        include("addNote.php");
         ?>
         <script src="js/script.js"></script>
         <!-- bootstrape js linkk -->
